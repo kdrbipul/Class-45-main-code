877e6af8245297555af089e420feee93ad3390b8
kdrbipul <kdrbipul@gmail.com>
2023-09-15 16:35:13 +0600

    react router dom add

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import {RouterProvider } from 'react-router-dom'
+import router from './Router/Router'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-     
+     <RouterProvider router={router}></RouterProvider>
     </>
   )
 }
---
+++ src/Firebase/firebase.init.js
@@ -0,0 +1,15 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDU3ZAy0K4yiJr4pMbP-ceVxZQrqFC5I4I",
+  authDomain: "my-first-firebase-projec-b5dd3.firebaseapp.com",
+  projectId: "my-first-firebase-projec-b5dd3",
+  storageBucket: "my-first-firebase-projec-b5dd3.appspot.com",
+  messagingSenderId: "659827444564",
+  appId: "1:659827444564:web:f1a8ae78519d4dc90996c1"
+};
+
+
+const app = initializeApp(firebaseConfig);
+export default app;
