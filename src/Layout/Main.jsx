63ec497b516f3308c7751fa211ac8e4224d31155
kdrbipul <kdrbipul@gmail.com>
2023-09-16 01:55:32 +0600

    github add

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import app from '../Firebase/firebase.init';
 
 const auth = getAuth(app);
@@ -11,6 +11,7 @@ const Main = () => {
     console.log(user);
 
     const googleProvider = new GoogleAuthProvider();
+    const githubProvider = new GithubAuthProvider();
 
     const handleGoogleLogin = () =>{
         // console.log('clicked the function');
@@ -22,10 +23,32 @@ const Main = () => {
         })
         .catch(error => console.log("Firebase Error :", error))
     }
+
+    const handleGitHubLogin = () =>{
+        // console.log('GitHud Login');
+        signInWithPopup(auth, githubProvider)
+        .then(result =>{
+            const user = result.user;
+            setUser(user)
+            console.log(user);
+        })
+        .catch(error => console.log("GitHub Error :", error))
+
+    }
+
+
     return (
         <div>
             <h1>Hello Firebase Are You Ready?</h1>
+            {
+                user.uid && <div>
+                    <h3>Name : {user?.displayName}</h3>
+                    <h3>Email : {user?.email}</h3>
+                    <img src={user?.photoURL} alt="" />
+                </div>
+            }
             <button onClick={handleGoogleLogin}>Login with Google</button>
+            <button onClick={handleGitHubLogin}>Login with GitHub</button>
         </div>
     );
 };
