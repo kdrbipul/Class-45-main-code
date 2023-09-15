825340361379298915566fa42482fa4258485fbc
kdrbipul <kdrbipul@gmail.com>
2023-09-15 18:08:20 +0600

    authentication add

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import app from '../Firebase/firebase.init';
+
+const auth = getAuth(app);
+
 
 const Main = () => {
+
+    const googleProvider = new GoogleAuthProvider();
+
+    const handleGoogleLogin = () =>{
+        // console.log('clicked the function');
+        signInWithPopup(auth, googleProvider)
+        .then(result =>{
+            const user = result.user;
+            console.log(user);
+        })
+        .catch(error => console.log("Firebase Error :", error))
+    }
     return (
         <div>
-            <h1>This is main part</h1>
+            <h1>Hello Firebase Are You Ready?</h1>
+            <button onClick={handleGoogleLogin}>Login with Google</button>
         </div>
     );
 };
